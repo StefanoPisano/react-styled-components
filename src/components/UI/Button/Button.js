522f415b572e02ddc3f7b3508b50064c953e8db7
Stefano Pisano <s.pisano@techgap.it>
2023-09-16 16:19:09 +0200

    chore: removes commented code

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -20,12 +20,5 @@ const Button = styled.button`
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
 }
 `
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
 
 export default Button;
